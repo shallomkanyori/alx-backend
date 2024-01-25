0d89b0a701eb0a33aebcae5647165498f704a2ec
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-25 07:29:53 +0000

    Add function that returns pagination index range
    Add a Python helper function that returns the index range for particular
    page and page_size pagination parameters.

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+"""Pagination: Simple helper function
+
+Functions:
+    index_range(page, page_size)
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns start and end index for pagination parameters.
+
+    Args:
+        page: The page to get the indices for.
+        page_size: The size of each page.
+    """
+    return (page_size * (page - 1), page_size * page)
