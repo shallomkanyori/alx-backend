c4a93b12a27e1a80f540bdeaca320197b90696b6
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-26 18:30:22 +0000

    Add leading slash in shebang

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """Pagination: Simple helper function
 
 Functions:
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """Pagination: Simple pagination
 
 Functions:
