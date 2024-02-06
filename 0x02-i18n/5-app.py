0951dafdb06746c990b242d870f13b1b2041cbcd
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-06 13:40:54 +0000

    Return correct template

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -63,7 +63,7 @@ def get_locale() -> Union[str, None]:
 @app.route("/")
 def home() -> str:
     """Returns a simple template"""
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
