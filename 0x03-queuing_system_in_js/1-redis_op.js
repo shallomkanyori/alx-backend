ec88d5b92f67a70a68574958b221dec69bc586a1
Shallom Kanyori <shallomkanyori@gmail.com>
2024-03-04 10:05:32 +0000

    Implement JS functions to set and get key in Redis

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+/**
+ * Connects to a Redis server
+ */
+import { createClient, print } from 'redis';
+
+const client = createClient();
+client.on('error', err => console.log('Redis client not connected to the server:', err.message));
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+/**
+ * Sets in Redis the value for the a key
+ * @param {string} schoolName The key to set a value for
+ * @param {*} value The value to set for the key
+ */
+function setNewSchool (schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+/**
+ * Logs to the console the value for the key passed in
+ * @param {string} schoolName The key to display the value for
+ */
+function displaySchoolValue (schoolName) {
+  client.get(schoolName, function (_, reply) {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
--- 0x03-queuing_system_in_js/README.md
+++ 0x03-queuing_system_in_js/README.md
@@ -48,3 +48,23 @@ Using Babel and ES6, write a script named `0-redis_client.js`. It should connect
 
 Requirements:
 - To import the library, you need to use the keyword `import`
+
+### Task 2
+In a file `1-redis_op.js`, copy the code you previously wrote (`0-redis_client.js`).
+
+Add two functions:
+- `setNewSchool`:
+	- It accepts two arguments `schoolName`, and `value`.
+	- It should set in Redis the value for the key `schoolName`
+	- It should display a confirmation message using `redis.print`
+- `displaySchoolValue`:
+	- It accepts one argument `schoolName`.
+	- It should log to the console the value for the key passed as argument
+
+At the end of the file, call:
+- `displaySchoolValue('Holberton');`
+- `setNewSchool('HolbertonSanFrancisco', '100');`
+- `displaySchoolValue('HolbertonSanFrancisco');`
+
+Requirements:
+- Use callbacks for any of the operation, we will look at async operations later
